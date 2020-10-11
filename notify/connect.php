815bc1f8d2120b61cf97c8ec6a48297d672b7c3a
varnikajain15 <55780559+varnikajain15@users.noreply.github.com>
2020-10-11 10:00:00 +0530

    Update connect.php

--- notify/connect.php
+++ notify/connect.php
@@ -1,4 +1,4 @@
 <?php 
 //connect.php;
-$connect = mysqli_connect("localhost", "root", "", "testing");
+$connect = mysqli_connect("ec2-54-160-18-230.compute-1.amazonaws.com", "orsivuswylgniq", "d415e10f95921f099e2c5d4f5ff18051f4f7670a039c170f19c48e0bef91fbfb", "dent6chqe4s7h");
 ?>
