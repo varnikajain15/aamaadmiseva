279078ab321ef837ea4ec6a245980e58ba41ad88
varnikajain15 <55780559+varnikajain15@users.noreply.github.com>
2020-10-11 18:26:01 +0530

    Update connect.php

--- Notify_chat/connect.php
+++ Notify_chat/connect.php
@@ -1,4 +1,4 @@
 <?php 
 //connect.php;
-$connect = mysqli_connect("ec2-54-160-18-230.compute-1.amazonaws.com", "orsivuswylgniq", "d415e10f95921f099e2c5d4f5ff18051f4f7670a039c170f19c48e0bef91fbfb", "dent6chqe4s7h");
+$connect = mysqli_connect("sql12.freesqldatabase.com", "sql12370063", "kYSL6Vl86w", "sql12370063");
 ?>
