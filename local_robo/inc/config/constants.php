027f7daf300367c2c257eb4d35eb88c1fa04e958
varnikajain15 <55780559+varnikajain15@users.noreply.github.com>
2020-10-11 23:36:23 +0530

    Update constants.php

--- local_robo/inc/config/constants.php
+++ local_robo/inc/config/constants.php
@@ -1,21 +1,21 @@
 <?php
 	// Root url for the site
-	define('ROOT_URL', 'http://localhost/localrobo/');
+	define('ROOT_URL', 'http://localhost/local_robo/');
 	
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=localhost;dbname=testing');
+	define('DSN', 'mysql:host=sql12.freesqldatabase.com;dbname=sql12370063');
 	
 	// Hostname
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'sql12.freesqldatabase.com');
 	
 	// DB user
-	define('DB_USER', 'root');
+	define('DB_USER', 'sql12370063');
 	
 	// DB password
-	define('DB_PASSWORD', '');
+	define('DB_PASSWORD', ' kYSL6Vl86w');
 	
 	// DB name
-	define('DB_NAME', 'testing');
+	define('DB_NAME', 'sql12370063');
 ?>
